bc458bd04284e4109d844d4b5f13584dc5bb243d
Enis Ceyhun Alp <enis.alp@epfl.ch>
2023-03-09 02:56:27 +0100

    cleaning up

--- conode/conode.go
+++ conode/conode.go
@@ -16,18 +16,6 @@ import (
 	"os"
 	"path"
 
-	_ "github.com/dedis/protean/compiler"
-	//_ "github.com/dedis/protean/easyneff"
-	//_ "github.com/dedis/protean/easyrand"
-	_ "github.com/dedis/protean/pristore"
-	_ "github.com/dedis/protean/state"
-
-	//_ "github.com/dedis/protean/threshold"
-
-	//_ "github.com/dedis/protean/dummy"
-	//_ "github.com/dedis/protean/tdh"
-	//_ "github.com/dedis/protean/sigver"
-
 	"go.dedis.ch/cothority/v3"
 	"go.dedis.ch/onet/v3/app"
 	"go.dedis.ch/onet/v3/cfgpath"
