1d0da247b64a1a19687ce8866f6894498961ff1e
Enis Ceyhun Alp <enis.alp@epfl.ch>
2023-04-18 02:12:42 +0200

    new plot & data processing code + csv files

--- experiments/scripts/prepare_data.sh
+++ experiments/scripts/prepare_data.sh
@@ -3,12 +3,16 @@
 #python3.11 process_data.py ../microbenchmarks/verify/test_data/vm_local/local_kv.csv -l kv > data/kv_local.csv
 #python3.11 process_data.py ../microbenchmarks/verify/test_data/vm_local/local_opc.csv -l opc > data/opc_local.csv
 
-python3.11 process_data.py ../microbenchmarks/verify/test_data/verify_kv.csv kv > data/kv.csv
-python3.11 process_data.py ../microbenchmarks/verify/test_data/verify_opc.csv opc > data/opc.csv
-python3.11 process_data.py ../microbenchmarks/sign/test_data/sign.csv sign > data/sign.csv
+#python3.11 process_data.py ../microbenchmarks/verify/test_data/verify_kv.csv kv > data/kv.csv
+python3.11 process_data.py ../microbenchmarks/verify/test_data/vopc_v1.csv opc > data/opc.csv
+python3.11 process_data.py ../microbenchmarks/sign/test_data/sign_v1.csv sign > data/sign.csv
 
 #python3.11 process_data.py ../microbenchmarks/shuffle/test_data/shuffle_regular.csv shuf > data/shuf_reg.csv
 #python3.11 process_data.py ../microbenchmarks/shuffle/test_data/shuffle_dfu.csv shuf > data/shuf_dfu.csv
 
 #python3.11 process_data.py ../microbenchmarks/threshold/test_data/threshold_regular.csv dec > data/dec_reg.csv
 #python3.11 process_data.py ../microbenchmarks/threshold/test_data/threshold_dfu.csv dec > data/dec_dfu.csv
+
+python3.11 process_data.py ../randlottery/test_data/randlottery_v1.csv rlot
+python3.11 process_data.py ../dkglottery/test_data/dkglottery_v0.csv dlot
+python3.11 process_data.py ../evoting/test_data/evoting_v0.csv evote
--- experiments/scripts/process_data.py
+++ experiments/scripts/process_data.py
@@ -2,6 +2,25 @@ import csv
 import sys
 import re
 import argparse
+import os
+import numpy as np
+
+jv_header = ['num_participants', 'avg', 'min', 'max']
+txn_header = ['num_participants', 'total']
+
+BASE_DIR = "data"
+
+DKGLOTTERY = "dkglottery"
+RANDLOTTERY = "randlottery"
+EVOTING = "evoting"
+SETUP = "setup"
+JOIN = "join"
+VOTE = "vote"
+CLOSE = "close"
+LOCK = "lock"
+SHUFFLE = "shuffle"
+TALLY = "tally"
+FINALIZE = "finalize"
 
 vpattern = 'verify_(\d+)_(\d+)_wall_avg'
 vpattern_min = 'verify_(\d+)_(\d+)_wall_min'
@@ -10,6 +29,15 @@ vpattern_max = 'verify_(\d+)_(\d+)_wall_max'
 vpattern_local = 'verify_local_(\d+)_(\d+)_wall_avg'
 
 spattern = 'sign_(\d+)_(\d+)_wall_avg'
+
+setup_pattern = "setup_(\w+)_wall_avg"
+lpattern = "lock_(\w+)_wall_avg"
+shuf_pattern = "shuffle_(\w+)_wall_avg"
+tpattern = "tally_(\w+)_wall_avg"
+cpattern = "close_(\w+)_wall_avg"
+fpattern = "finalize_(\w+)_wall_avg"
+
+cwd = os.getcwd()
 
 def read_data(fname):
     with open(fname, 'r') as f:
@@ -98,22 +126,67 @@ def process_dfu_mb(data_read, prefix):
     for k in data:
         print("%d,%6f" % (k,data[k]))
 
-def process_evote(data_read):
-    data = dict()
+def process_jv(data_read, app_type, txn_type):
+    latency_vals = dict()
     for dr in data_read:
         num_participants = int(dr['numparticipants'])
-        vals = list()
-        for i in range(num_participants):
-            vals.append(float(dr[f'p{i}_vote_wall_avg']))
-        data[num_participants] = vals
+        if num_participants > 5:
+            vals = list()
+            for i in range(num_participants):
+                val = float(dr[f'p{i}_{txn_type}_wall_avg'])
+                vals.append(val)
+                # print(i, val)
+            latency_vals[num_participants] = vals
 
-    for k in data:
-        data[k].sort()
-        print("%d" % (k), end='')
-        for v in data[k]:
-            print(",%6f" % (v), end='')
-        print()
+    fpath = os.path.join(cwd, BASE_DIR, app_type, f'{txn_type}.csv')
+    with open(fpath, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(jv_header)
+        for k in latency_vals:
+            vals = np.array(latency_vals[k])
+            min_val = vals.min()
+            max_val = vals.max()
+            mean_val = vals.mean()
+            data = [k, mean_val, min_val, max_val]
+            writer.writerow(data)
+            # print("%d,%.6f,%.6f,%.6f" % (k, mean_val, min_val, max_val))
 
+def process_txn(data_read, app_type, txn_type, pattern):
+    print(">>>", txn_type)
+    fpath = os.path.join(cwd, BASE_DIR, app_type, f'{txn_type}.csv')
+    with open(fpath, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(txn_header)
+        data = dict()
+        for dr in data_read:
+            num_participants = int(dr['numparticipants'])
+            if num_participants > 5:
+                total = 0.0
+                for label, value in dr.items():
+                    match = re.search(pattern, label)
+                    if match is not None and match.group(1) != "getstate":
+                        total += float(value)
+                data = [num_participants, total]
+                writer.writerow(data)
+                print("%d,%.6f" % (num_participants, total))
+
+def process_randlot(data_read):
+    process_jv(data_read, RANDLOTTERY, JOIN)
+    process_txn(data_read, RANDLOTTERY, CLOSE, cpattern)
+    process_txn(data_read, RANDLOTTERY, FINALIZE, fpattern)
+
+def process_dkglot(data_read):
+    process_jv(data_read, DKGLOTTERY, JOIN)
+    process_txn(data_read, DKGLOTTERY, SETUP, setup_pattern)
+    process_txn(data_read, DKGLOTTERY, CLOSE, cpattern)
+    process_txn(data_read, DKGLOTTERY, FINALIZE, fpattern)
+
+def process_evote(data_read):
+    process_jv(data_read, EVOTING, VOTE)
+    process_txn(data_read, EVOTING, SETUP, setup_pattern)
+    process_txn(data_read, EVOTING, LOCK, lpattern)
+    process_txn(data_read, EVOTING, SHUFFLE, shuf_pattern)
+    process_txn(data_read, EVOTING, TALLY, tpattern)
 
 def dump_kv(data_read):
     data = dict()
@@ -161,7 +234,7 @@ def main():
     parser = argparse.ArgumentParser(description='Parsing csv files')
     parser.add_argument('fname', type=str)
     parser.add_argument('exp_type', choices=['kv', 'opc', 'sign', 'shuf',
-                                             'dec', 'evote'], type=str)
+                                             'dec', 'rlot', 'dlot', 'evote'], type=str)
     parser.add_argument('-l', dest='local', action='store_true')
     parser.add_argument('-d', dest='dump', action='store_true')
     args = parser.parse_args()
@@ -182,6 +255,10 @@ def main():
         process_dfu_mb(data_read, "shuffle")
     elif "dec" in args.exp_type:
         process_dfu_mb(data_read, "decrypt")
+    elif "rlot" in args.exp_type:
+    		process_randlot(data_read)
+    elif "dlot" in args.exp_type:
+    		process_dkglot(data_read)
     elif "evote" in args.exp_type:
         process_evote(data_read)
 
